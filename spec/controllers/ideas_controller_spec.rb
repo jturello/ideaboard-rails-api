af956e79c7a4260d37e092835b2d7c05f57efaef
James Turello <jturello@yahoo.com>
2017-10-24 17:28:40 -0700

    add resources/routes and specs for idea model

---
+++ spec/controllers/ideas_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe IdeasController, type: :controller do
+
+  describe "GET #index" do
+    
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns a saved idea' do
+      @idea = create :idea, title: 'Newly saved idea!'
+      get :index
+      expect(JSON.parse(response.body).first["title"]).to eq('Newly saved idea!')
+    end
+  end
+end
