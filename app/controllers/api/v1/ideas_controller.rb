bbba48b751f900e4931f2c605fb8f0987ff82650
James Turello <jturello@yahoo.com>
2017-10-31 18:55:09 -0700

    add create controller action

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -4,5 +4,20 @@ module Api::V1
       @ideas = Idea.all
       render json: @ideas 
     end
+
+    def create 
+      @idea = Idea.new(idea_params)
+      if @idea.save 
+	render json: @idea
+      else
+	render json: @idea.errors, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def idea_params
+      params.require(:idea).permit(:title, :body)
+    end
   end
 end
